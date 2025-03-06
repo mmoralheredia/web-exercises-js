8720b09a0220f0a851015445098bc146c427b5f9
mmoralheredia <maikel.moral232@gmail.com>
2025-03-05 23:29:43 -0500

    feat: add an exercise and add styles to the main page

---
+++ JavascriptProjects/003-OurReview/src/index.js
@@ -0,0 +1,21 @@
+const users = [
+    {
+        id: 1,
+        name: 'Amando Capece',
+        img: './users/AmadoCapece.jpg',
+        review: 'Esta es una buena característica para tener en un sitio de comercio electrónico para mostrar comentarios de clientes o un portafolio personal para mostrar comentarios de clientes.'
+    },
+    {
+        id: 1,
+        name: 'Antoine Sweezy',
+        img: './users/AntoineSweezy.jpg',
+        review: 'Esto es otro texto de review de un negocio este es el de Antoine, seria una pequeña review.'
+    },
+    {
+        id: 1,
+        name: 'Eun Andreacchio',
+        img: './users/EunAndreacchio.jpg.jpg',
+        review: 'creo que el producto es de buena factura, eso esta bastante bien pero podría mejorar ciertas funcionalidades, para que sea mejor de usar'
+    },
+
+]
